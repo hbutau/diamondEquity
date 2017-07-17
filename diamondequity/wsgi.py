1c323090950b17314fbbbec869ac389aca362fac
h3zTr0n <alison.mukoma@yahoo.com>
2017-07-17 08:13:13 +0200

    cleaning up

--- diamondequity/wsgi.py
+++ diamondequity/wsgi.py
@@ -10,10 +10,8 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "diamondequity.settings")
 
 from whitenoise.django import DjangoWhiteNoise
-
 application = get_wsgi_application()
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "diamondequity.settings")
 application = DjangoWhiteNoise(application)
