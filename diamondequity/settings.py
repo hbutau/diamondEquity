d545c488fb5e7584166f19f4309e2724c6247cf4
h3zTr0n <alison.mukoma@yahoo.com>
2017-07-17 08:37:05 +0200

    struggling to have staticfiles render on heroku

--- diamondequity/settings.py
+++ diamondequity/settings.py
@@ -122,13 +122,13 @@ ALLOWED_HOSTS = ['*']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
     os.path.join(PROJECT_ROOT, 'static'),
 ]
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
