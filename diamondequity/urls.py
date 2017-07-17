55e8b36da77f731e1c8485e0ebcebf27065f34b0
h3zTr0n <alison.mukoma@yahoo.com>
2017-07-17 09:21:11 +0200

    added django static serve function

--- diamondequity/urls.py
+++ diamondequity/urls.py
@@ -21,8 +21,10 @@ from django.contrib import admin
 from pages import urls as pages_urls
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include(pages_urls, namespace='pages')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+urlpatterns += staticfiles_urlpatterns()
